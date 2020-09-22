326c7fde251615aa80d71411496aa29094a890a7
ryoma1200 <ryoma12sandasm@gmail.com>
2020-09-22 17:30:41 +0900

    クエリの訂正 #10

--- lamp_dock/append_schema.sql
+++ lamp_dock/append_schema.sql
@@ -1,4 +1,3 @@
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 
@@ -9,42 +8,49 @@ SET time_zone = "+00:00";
 --  -----------------------------------------------
 
 
-CREATE TABLE `history` (
-    `history_id` INT(11) NOT NULL,
+CREATE TABLE `order` (
+    `order_id` INT(11) NOT NULL,
     `user_id` INT(11) NOT NULL,
-    `order_num` INT(11) NOT NULL,   /* histryテーブルへの追加処理の直前に、以前のorder_numを取得し、それに１加算したものをこちらに代入する */
     `create_date` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
-ALTER TABLE `history`
-    ADD PRIMARY KEY (`history_id`),
+ALTER TABLE `order`
+    ADD PRIMARY KEY (`order_id`),
     ADD KEY `user_id` (`user_id`);
 
 
-ALTER TABLE `history`
-    MODIFY `history_id` INT(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+ALTER TABLE `order`
+    MODIFY `order_id` INT(11) NOT NULL AUTO_INCREMENT;
 
 
 --  -----------------------------------------------
 
 
-CREATE TABLE `payment_statement` (
-    `statement_id` INT(11) NOT NULL,
-    `order_num` INT(11) NOT NULL,
+CREATE TABLE `order_item` (
+    `order_item_id` INT(11) NOT NULL,
+    `order_id` INT(11) NOT NULL,
     `item_id` INT(11) NOT NULL,
     `price` INT(11) NOT NULL,
     `amount` INT(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
-ALTER TABLE `payment_statement`
-    ADD PRIMARY KEY (`statement_id`),
+ALTER TABLE `order_item`
+    ADD PRIMARY KEY (`order_item_id`),
+    ADD KEY `order_id` (`order_id`),
     ADD KEY `item_id` (`item_id`); 
 
 
-ALTER TABLE `payment_statement`
-    MODIFY `statement_id` INT(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+ALTER TABLE `order_item`
+    ADD CONSTRAINT `restricted_order_id`        -- 制約の名前
+    FOREIGN KEY (`order_id`)        -- どのキーに制約をつけるか
+    REFERENCES `order` (`order_id`)        -- どのテーブルのどのカラムを参照するか
+    ON DELETE RESTRICT ON UPDATE RESTRICT;       -- 参照元を delete or update しようとするとエラーになる
+
+
+ALTER TABLE `order_item`
+    MODIFY `order_item_id` INT(11) NOT NULL AUTO_INCREMENT;
 
 
 COMMIT;
